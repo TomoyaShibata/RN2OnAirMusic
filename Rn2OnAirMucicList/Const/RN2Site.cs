027e8a83da02a6ebe1fc5ff524c71fbc982b5280
tomoyashibata <wind.of.hometown@gmail.com>
2015-02-26 09:31:15 +0900

    [modify] 正規表現のソース文字列を定数で持つことをやめ、はじめから正規表現として持つ形に変更 [modify] RN2フィード本文に対するオンエア時間部、または曲タイトル/フィードの文字列か否かのチェックメソッドを追加（未検証）

--- Rn2OnAirMucicList/Const/RN2Site.cs
+++ Rn2OnAirMucicList/Const/RN2Site.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace Rn2OnAirMucicList.Const {
 	public class RN2Site {
-		public const string FEED_URI   = "http://www.radionikkei.jp/musicrn2/rss2/";
-		public const string TIME_REGEX = @"＜.*＞$"; // ＜ で始まり ＞ で終わる単語に一致
-		public const string MUSIC_INFO_DELIMITER= @"/";
+		public const string FEED_URI             = "http://www.radionikkei.jp/musicrn2/rss2/";
+		public const string MUSIC_INFO_DELIMITER = @"/";
+		public const Regex  ONAIR_TIME_RGX        = new Regex(@"＜.*＞$"); // ＜ で始まり ＞ で終わる単語に一致
+
 	}
 }
--- Rn2OnAirMucicList/Models/RN2Feed.cs
+++ Rn2OnAirMucicList/Models/RN2Feed.cs
@@ -19,7 +19,7 @@ namespace Rn2OnAirMucicList.Models {
 		public SyndicationFeed GetFeed() {
 			var feeds = new List<String>();
 			using (XmlReader xmlReader = XmlReader.Create(Const.RN2Site.FEED_URI)) {
-				var feed = SyndicationFeed.Load(xmlReader);
+				var feed      = SyndicationFeed.Load(xmlReader);
 				var feedTexts = new List<String>();
 				foreach (SyndicationItem item in feed.Items) {
 					feedTexts.Add(item.Summary.Text);
@@ -28,11 +28,31 @@ namespace Rn2OnAirMucicList.Models {
 				var delimiter = new String[]{"<br />"};
 				feedTexts.ForEach(t => {
 					var l = t.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+					Debug.WriteLine(IsOnairTimeText(t));
+					Debug.WriteLine(IsMucicInfoText(t));
 					Debug.WriteLine(string.Join("", t));
 				});
 
 				return feed;
 			}
+		}
+
+		/// <summary>
+		/// 値が RN2 オンエア楽曲フィード本文の時間部かどうかチェックする
+		/// </summary>
+		/// <param name="text">フィードから抽出した1行</param>
+		/// <returns></returns>
+		private bool IsOnairTimeText(String text) {
+			return Const.RN2Site.ONAIR_TIME_RGX.IsMatch(text);
+		}
+
+		/// <summary>
+		/// 値が RN2 オンエア楽曲フィード本文の曲タイトル/アーティスト部かどうかチェックする
+		/// </summary>
+		/// <param name="text">フィードから抽出した1行</param>
+		/// <returns></returns>
+		private bool IsMucicInfoText(String text) {
+			return text.Contains(Const.RN2Site.MUSIC_INFO_DELIMITER);
 		}
 	}
 }
